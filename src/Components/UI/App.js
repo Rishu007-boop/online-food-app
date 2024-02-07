fd93b2a5e23fa14476d798d94786d91fcb75ac05
Ram Mohan Singh <singhrammohan321@gmail.com>
2024-02-07 16:13:03 -0500

    small changes

--- src/Components/UI/App.js
+++ src/Components/UI/App.js
@@ -3,6 +3,7 @@ import Header from "../Layouts/Header";
 import Cart from "../Cart/Cart";
 import CartProvider from "../../store/CartProvider";
 import Meals from "../Meals/Meals";
+import "../../App.css"
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -19,7 +20,7 @@ function App() {
     <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
-      <main>
+      <main className="app-main">
         <Meals />
       </main>
     </CartProvider>
